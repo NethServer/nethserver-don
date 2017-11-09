ee4b50e253984647825e8618f1cc5203d7ec7b02
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2017-11-09 10:53:11 +0100

    refactor and rename sos to don

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Don.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Don.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
-$L['NethSos_Title'] = 'Remote support';
-$L['NethSos_Description'] = 'Start a secure channel for Nethesis remote support';
-$L['NethSos_Tags'] = 'helpdesk remote access help support issue problem bug';
+$L['Don_Title'] = 'Remote support';
+$L['Don_Description'] = 'Start a secure channel for remote support';
+$L['Don_Tags'] = 'helpdesk remote access help support issue problem bug';
 
-$L['NethSos_Start_header'] = 'Start a remote support session';
-$L['NethSos_Stop_header'] = 'Stop the current support session';
+$L['Don_Start_header'] = 'Start a remote support session';
+$L['Don_Stop_header'] = 'Stop the current support session';
 $L['Start_label'] = 'Start';
 $L['Stop_label'] = 'Stop';
 $L['SessionDuration_label'] = 'Expire after (days): ${0}';
---
+++ root/usr/share/nethesis/NethServer/Module/Don/Stop.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * Copyright (C) 2017 Nethesis S.r.l.
+ * http://www.nethesis.it - nethserver@nethesis.it
+ *
+ * This script is part of NethServer.
+ *
+ * NethServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * NethServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NethServer.  If not, see COPYING.
+ */
+
+namespace NethServer\Module\Don;
+use Nethgui\System\PlatformInterface as Validate;
+
+class Stop extends \Nethgui\Controller\AbstractController
+{
+    public function initialize()
+    {
+        $self = $this;
+        $sessionIdAdapter = $this->getPlatform()->getMapAdapter(function()use($self){
+            $contents = file('/run/don/credentials');
+            return trim($contents[1]);
+        });
+        parent::initialize();
+        $this->declareParameter('ServerId', FALSE, array('configuration', 'nethupdate', 'SystemID'));
+        $this->declareParameter('SessionId', FALSE, $sessionIdAdapter);
+        $this->declareParameter('SessionExpire', FALSE);
+    }
+
+    public function process()
+    {
+        if($this->getRequest()->isMutation()) {
+            $this->getPlatform()->signalEvent('nethserver-don-stop &');
+        }
+        parent::process();
+    }
+    public function prepareView(\Nethgui\View\ViewInterface $view)
+    {
+        parent::prepareView($view);
+        if($this->getRequest()->isValidated()) {
+             $view->getCommandList()->show();
+        }
+    }
+    public function nextPath()
+    {
+        if($this->getRequest()->isMutation()) {
+            return 'Start';
+        }
+        return FALSE;
+    }
+}
